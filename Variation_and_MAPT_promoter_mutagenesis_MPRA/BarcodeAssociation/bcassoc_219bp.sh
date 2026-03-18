0df8c3f40d3bfa484ffe86707d9ee58f001f8076
beckyhauser <111378627+beckyhauser@users.noreply.github.com>
2026-03-18 15:21:35 -0500

    Create bcassoc_219bp.sh

---
+++ Variation_and_MAPT_promoter_mutagenesis_MPRA/BarcodeAssociation/bcassoc_219bp.sh
@@ -0,0 +1,45 @@
+  GNU nano 5.6.1                                                                                              bcassoc219_onlycalledreadsshorttime2_mapq0cigar.sh                                                                                                         
+#! /bin/bash
+#SBATCH -p normal
+#SBATCH -o MPRAflow_assoc_219_called_shorttime2_mapq0cigar_%j.out
+
+
+###change these variables###
+MPRAflowDir="/cluster/home/rhauser/MPRAflow"
+workingDir="/cluster/home/rhauser/files/rmhex03/bcassoc/bcassoc219/workingcalled_mapq0cigar"
+outputDir="/cluster/home/rhauser/files/rmhex03/bcassoc/bcassoc219/outputcalled_mapq0cigar"
+read1="/cluster/home/rhauser/files/rmhex03/bcassoc/seq/fastqs/RMHex03_bcassoc/RMHex03_bcassoc_i741_S1_R1_001.fastq.gz"
+read2="/cluster/home/rhauser/files/rmhex03/bcassoc/seq/fastqs/RMHex03_bcassoc/RMHex03_bcassoc_i741_S1_R3_001.fastq.gz"
+bcRead="/cluster/home/rhauser/files/rmhex03/bcassoc/seq/fastqs/RMHex03_bcassoc/RMHex03_bcassoc_i741_S1_R2_001.fastq.gz"
+designFile="/cluster/home/rhauser/files/rmhex03/design/varmpradesign_219bp.fasta"
+name="assoc_basic_219_called_shorttime_mapq0cigar"
+cigar="219M"
+
+######
+
+cd $MPRAflowDir
+
+set +eu
+export HOST=$(hostname)
+source /cluster/home/rhauser/miniconda3/etc/profile.d/conda.sh
+conda activate /cluster/home/rhauser/miniconda3/envs/MPRAflow
+
+nextflow run association_rmhedits2_shorttime2.nf --mapq 0 --cigar 219M --w $workingDir --fastq-insert $read1 --fastq-insertPE $read2 --fastq-bc $bcRead --design $designFile --name $name --outdir $outputDir
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
