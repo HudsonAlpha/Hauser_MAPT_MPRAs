973f2857d098a9f0fd14bfc5d5d1ca51da0df909
beckyhauser <111378627+beckyhauser@users.noreply.github.com>
2026-03-18 15:27:08 -0500

    Create bcassoc208bp.sh

---
+++ Variation_and_MAPT_promoter_mutagenesis_MPRA/BarcodeAssociation/bcassoc208bp.sh
@@ -0,0 +1,36 @@
+  GNU nano 5.6.1                                                                                              bcassoc208_onlycalledreadsshorttime2_mapq0cigar.sh                                                                                                         
+#! /bin/bash
+#SBATCH -p normal
+#SBATCH -o MPRAflow_assoc_208_called_shorttime2mapq_%j.out
+
+
+###change these variables###
+workingDir="/cluster/home/rhauser/files/rmhex03/bcassoc/bcassoc208/workingcalledmapq"
+outputDir="/cluster/home/rhauser/files/rmhex03/bcassoc/bcassoc208/outputcalledmapq"
+designFile="/cluster/home/rhauser/files/rmhex03/design/varmpradesign_208bp.fasta"
+name="assoc_basic_208_called_shorttime2_mapq"
+cigar="208M"
+read1="/cluster/home/rhauser/files/rmhex03/bcassoc/seq/fastqs/RMHex03_bcassoc/RMHex03_bcassoc_i741_S1_R1_001.fastq.gz"
+read2="/cluster/home/rhauser/files/rmhex03/bcassoc/seq/fastqs/RMHex03_bcassoc/RMHex03_bcassoc_i741_S1_R3_001.fastq.gz"
+bcRead="/cluster/home/rhauser/files/rmhex03/bcassoc/seq/fastqs/RMHex03_bcassoc/RMHex03_bcassoc_i741_S1_R2_001.fastq.gz"
+MPRAflowDir="/cluster/home/rhauser/MPRAflow"
+
+######
+
+cd $MPRAflowDir
+
+set +eu
+export HOST=$(hostname)
+source /cluster/home/rhauser/miniconda3/etc/profile.d/conda.sh
+conda activate /cluster/home/rhauser/miniconda3/envs/MPRAflow
+
+nextflow run association_rmhedits2_shorttime2.nf --mapq 0 --cigar $cigar --w $workingDir --fastq-insert $read1 --fastq-insertPE $read2 --fastq-bc $bcRead --design $designFile --name $name --outdir $outputDir
+
+
+
+
+
+
+
+
+
